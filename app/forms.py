10dc1bc09a00b93004d368dacdb2fdf2d6267521
Anastasia <anastasiya.lomakina.2004@mail.ru>
2025-06-16 18:36:10 +0300

    forms

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, DecimalField, DateField, SelectField, RadioField
+from wtforms.validators import DataRequired, Length, ValidationError
+from flask_wtf.file import FileField, FileAllowed 
+from wtforms_sqlalchemy.fields import QuerySelectMultipleField
+from wtforms.widgets import ListWidget, CheckboxInput
+
+from app.models import Equipment, ResponsiblePerson
 
 class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired(message="Пожалуйста, введите логин.")])
@@ -9,3 +14,35 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Запомнить меня')
 
     submit = SubmitField('Войти')
+
+def get_responsible_person_label(person):
+    return person.full_name
+
+class EquipmentForm(FlaskForm):
+    name = StringField('Название', validators=[DataRequired(), Length(min=3, max=200)])
+    inventory_number = StringField('Инвентарный номер', validators=[DataRequired(), Length(min=1, max=100)])
+    category = SelectField('Категория', coerce=int, validators=[DataRequired()])
+    purchase_date = DateField('Дата покупки', format='%Y-%m-%d', validators=[DataRequired()])
+    cost = DecimalField('Стоимость', validators=[DataRequired()])
+    status = RadioField('Статус', choices=[
+        ('В эксплуатации', 'В эксплуатации'),
+        ('На ремонте', 'На ремонте'),
+        ('Списано', 'Списано')
+    ], validators=[DataRequired()])
+    notes = TextAreaField('Примечание')
+    responsible_persons = QuerySelectMultipleField(
+        'Ответственные лица',
+        query_factory=lambda: ResponsiblePerson.query.order_by('full_name'),
+        get_label=get_responsible_person_label,
+        allow_blank=True,
+    )
+    
+    photo = FileField('Фотография', validators=[
+        FileAllowed(['jpg', 'jpeg', 'png'], 'Разрешены только изображения!')
+    ])
+    submit = SubmitField('Сохранить')
+
+    def validate_inventory_number(self, inventory_number):
+        equipment = Equipment.query.filter_by(inventory_number=inventory_number.data).first()
+        if equipment:
+            raise ValidationError('Оборудование с таким инвентарным номером уже существует.')
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,23 @@
-from app.models import Equipment, User, ServiceHistory
-from flask import render_template, Blueprint, redirect, url_for, flash, request
+from app.models import Equipment, User, ServiceHistory, Category, Image
+from flask import render_template, Blueprint, redirect, url_for, flash, request, abort, current_app, send_from_directory
 from flask_login import current_user, login_user, logout_user, login_required
-from app.forms import LoginForm
+from app.forms import LoginForm, EquipmentForm
+from functools import wraps
+from app import db 
+from werkzeug.utils import secure_filename
+import os
+import hashlib
 
 bp = Blueprint('main', __name__)
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_admin():
+            flash('У вас недостаточно прав для выполнения данного действия.', 'danger')
+            return redirect(url_for('main.index'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @bp.route('/')
 @bp.route('/index')
@@ -51,3 +65,66 @@ def equipment_detail(id):
         equipment=equipment_item,
         history=service_history 
     )
+
+def save_picture(form_picture):
+    md5 = hashlib.md5(form_picture.read()).hexdigest()
+    form_picture.seek(0)
+    image = Image.query.filter_by(md5_hash=md5).first()
+    if image:
+        return image
+    file_ext = os.path.splitext(form_picture.filename)[1]
+    filename = md5 + file_ext
+
+    picture_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+    form_picture.save(picture_path)
+    
+    new_image = Image(
+        filename=filename,
+        mime_type=form_picture.mimetype,
+        md5_hash=md5
+    )
+    db.session.add(new_image)
+    db.session.commit() 
+    
+    return new_image
+
+@bp.route('/equipment/add', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def add_equipment():
+    form = EquipmentForm()
+    form.category.choices = [(c.id, c.name) for c in Category.query.order_by('name').all()]
+    
+    if form.validate_on_submit():
+        image_id = None
+        if form.photo.data:
+            try:
+                image_obj = save_picture(form.photo.data)
+                image_id = image_obj.id
+            except Exception as e:
+                flash(f'При сохранении файла возникла ошибка: {e}', 'danger')
+                db.session.rollback()
+                return render_template('equipment_form.html', title='Добавление оборудования', form=form)
+        
+        new_equipment = Equipment(
+            name=form.name.data,
+            inventory_number=form.inventory_number.data,
+            category_id=form.category.data,
+            purchase_date=form.purchase_date.data,
+            cost=form.cost.data,
+            status=form.status.data,
+            notes=form.notes.data,
+            responsible_persons=form.responsible_persons.data,
+            image_id=image_id 
+        )
+        db.session.add(new_equipment)
+        db.session.commit()
+        
+        flash('Новое оборудование успешно добавлено!', 'success')
+        return redirect(url_for('main.equipment_detail', id=new_equipment.id))
+        
+    return render_template('equipment_form.html', title='Добавление оборудования', form=form)
+
+@bp.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(current_app.config['UPLOAD_FOLDER'], filename)
